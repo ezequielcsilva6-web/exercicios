5d7bf98b516620d62142649bbdb5bccafc6d9a23
ezequielcsilva6-web <ezequiel.c.silva6@ba.estudante.senai.br>
2026-03-26 21:39:59 -0300

    Add files via upload

---
+++ projeto/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Mensagem from './components/Mensagem'
+
+function App() {
+
+  return (
+    <>
+    <h1> Bem-vindo ao meu projeto!</h1>
+    
+    <Mensagem />
+    <Mensagem />
+    <Mensagem />
+    <Mensagem />
+    <Mensagem />
+    <Mensagem />
+    </>
+  )
+}
+
+export default App
---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export default function Mensagem() {
+    return (
+        <div className= 'mensagem'>
+            <h1>Componente</h1>
+            <p>Este é um componete simples.</p>
+        </div>
+    )
+}
+
+//export default Mensagem
